15306cb512041527c82663fd36148654bf3d9220
Unknown <marco.steffen@students.fhnw.ch>
2018-05-03 20:04:33 +0200

    Figure close (Kreuz) führt jetzt zum Menu zurück

--- source/Pong/pongengine.m
+++ source/Pong/pongengine.m
@@ -5,14 +5,20 @@ fig=figure
 plot(3,3)
 
 title (app.NickPlayer2EditField.Value)
-set(fig,'KeyPressFcn',@keyDown)
+set(fig,'KeyPressFcn',@keyDown)  %Weisst die KeyPressFcn der Figur zu
 
-    function keyDown(src,event)
+    function keyDown(src,event)  %Wertet den event Key aus.
         switch event.Key
-            case 'q'
-                app.Pong.Visible='on'
-                close(gcf)
+            case 'q'             %Taste q
+                
+                %app.Pong.Visible='on'
+                close(gcf)       %schliesst die Figur
         end
+    end
+set(fig, 'DeleteFcn', @figureclose) 
+
+    function figureclose(src,event)
+        app.Pong.Visible='on'
     end
 
 end

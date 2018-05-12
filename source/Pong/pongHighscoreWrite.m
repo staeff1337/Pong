a3d0fcf59de5ddf49895c21c92aadcafd5812d35
Unknown <marco.steffen@students.fhnw.ch>
2018-05-12 19:22:02 +0200

    HighscoreWrite

---
+++ source/Pong/pongHighscoreWrite.m
@@ -0,0 +1,11 @@
+function   pongHighscoreWrite(FileName,Name,Score)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+         fid = fopen(FileName,'a');    %csv Datei öffnen im Modus 'a' für Anhängen von Inhalt.
+         fprintf(fid,'%s %d', Name, Score);
+         fprintf(fid, '\n' );
+         fclose(fid); 
+        
+end
+
